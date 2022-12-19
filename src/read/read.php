ddb74f7b3801daf224fab23a45792aed149dbeea
Albert Arques <albertarques@gmail.com>
2022-12-19 12:43:09 +0100

    Add read recipes

---
+++ src/read/read.php
@@ -0,0 +1,35 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "albert";
+$tableName = "recetas";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->prepare('SELECT * FROM ' . $tableName);
+  $stmt->execute();
+
+  // set the resulting array to associative
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  $data = $stmt->fetchAll();
+  foreach ($data as $row) {
+    echo
+    '<div class="card" style="width: 18rem;">
+        <img src="' . $row["picture"] . '" class="card-img-top" alt="' . $row["nombre"] . '">
+      <div class="card-body">
+        <h5 class="card-title">' . $row["nombre"] . '</h5>
+        <p class="card-text">' . $row["tipo de dieta"] . '</p>
+        <p class="card-text">' . $row["pais de origen"] . '</p>
+        <p class="card-text">' . $row["contenido calorico"] . '</p>
+      </div>
+      <button class="btn btn-primary">Edit</button>
+    </div>';
+  }
+} catch (PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
+
+$conn = null;
