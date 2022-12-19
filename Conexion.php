ec1da0407ec5c6c69d57a9f7f9125a4c9ecf1c17
AlessHub <aless312@gmail.com>
2022-12-19 12:31:38 +0100

    added update function

--- Conexion.php
+++ Conexion.php
@@ -3,18 +3,17 @@
 class Conexion 
 {
     public $host = "localhost";
-    public $user = "root";
-    public $password = "";
-    public $db = "tutorial";
+    public $user = "messi";
+    public $password = "argentina";
+    public $db = "recetas";
     // we are created $connect to use for sql instructions
-    public $connect;
 
     public function __construct(){
         $connectionString="mysql:host =".$this->host.";dbname=".$this->db.";charset=utf8";
         try {
             $this->connect = new PDO($connectionString,$this->user,$this->password);
             $this->connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Conexion exitosa ju ju ju ";
+            // echo "Conexion exitosa ju ju ju ";
         } catch (Exception $e){
             $this->connect = 'Error de conexión';
             echo "ERROR: ".$e->getMessage();
--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <!doctype html>
 <html lang="en">
+<?php 
+include("./src/update/update.php");
+require_once("./Conexion.php");
 
+if(isset($_POST['update']))
+{
+    $connection = new Conexion();
+    $db = $connection->connect();
+    $name = $_POST['nombre'];
+    $origen = $_POST['origen'];
+    $tipo = $_POST['tipo'];
+    $calorias = $_POST['calorias'];
+    $imagen = $_POST['img'];
+    $update = new Update();
+    $updateFunction = $update->update($name, $origen, $tipo, $calorias, $imagen);
+    $result = $db->query($updateFunction);
+    // $result = mysqli_query($connection, $updateFunction);
+}
+
+
+
+?>
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -13,40 +34,43 @@
 </head>
 
 <body>
-    <h1>COMIDA</h1>
 
-    <form>
+    <h1>Editar</h1>
+  
+
+    <form method="post">
         <div class="mb-3">
             <label for="nombre" class="form-label">Nombre</label>
-            <input type="text" class="form-control" id="nombre">
+            <input type="text" class="form-control" name="nombre" id="nombre">
 
         </div>
         <div class="mb-3">
             <label for="origen" class="form-label">Origen</label>
-            <input type="text" class="form-control" id="origen">
+            <input type="text" class="form-control" name="origen" id="origen">
 
         </div>
         <div class="mb-3">
             <label for="tipo" class="form-label">Tipo</label>
-            <input type="text" class="form-control" id="tipo">
+            <input type="text" class="form-control" name="tipo" id="tipo">
         </div>
         <div class="mb-3">
             <label for="calorias" class="form-label">Calorias</label>
-            <input type="text" class="form-control" id="calorias">
+            <input type="text" class="form-control" name="calorias" id="calorias">
 
         </div>
         <div class="mb-3">
             <label for="img" class="form-label">Img</label>
-            <input class="form-control" type="file" id="img">
+            <input class="form-control" type="file" name="img" id="img">
         </div>
-
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" name="update" class="btn btn-primary">Update</button>
     </form>
 
     <section>Read</section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <table class="table">
 
-   
+  
+</table>
     
 </body>
 
